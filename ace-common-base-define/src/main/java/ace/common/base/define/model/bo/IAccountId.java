c88fc974ea86fbbea90e5cedc641cad617cdb459
caspar <279397942@qq.com>
2020-08-10 11:33:38 +0800

    init

---
+++ ace-common-base-define/src/main/java/ace/common/base/define/model/bo/IAccountId.java
@@ -0,0 +1,14 @@
+package ace.common.base.define.model.bo;
+
+/**
+ * @author Caspar
+ * @contract 279397942@qq.com
+ * @create 2020/7/26 13:15
+ * @description 构架自动注入账号ID accountId字段
+ */
+public interface IAccountId {
+
+    String getAccountId();
+
+    void setAccountId(String accountId);
+}
--- ace-common-base-define/src/main/java/ace/common/base/define/model/bo/IAppId.java
+++ ace-common-base-define/src/main/java/ace/common/base/define/model/bo/IAppId.java
@@ -4,7 +4,7 @@ package ace.common.base.define.model.bo;
  * @author Caspar
  * @contract 279397942@qq.com
  * @create 2020/7/26 13:15
- * @description
+ * @description 构架自动注册appId
  */
 public interface IAppId {
 
